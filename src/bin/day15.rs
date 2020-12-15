dcea81e1ffe0cdc09030c8fa16b898852e6a72ba
Koen Klaren <koen.klaren@gmail.com>
2020-12-15 09:24:40 +0100

    Day 15 (improved from ~2s to ~0.5s)

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -3,7 +3,6 @@ use std::io::Read;
 
 use itertools::Itertools;
 use std::time::Instant;
-use rustc_hash::FxHashMap;
 
 fn main() {
     let mut file = File::open("input/day15.txt").unwrap();
@@ -20,20 +19,20 @@ fn main() {
 
 fn compute_answer(starting_numbers: &Vec<u32>, target: usize) -> u32 {
     let mut prev_num: u32 = 0;
-    let mut last_spoken: FxHashMap<u32, u32> = FxHashMap::default();
+    let mut last_spoken = vec![u32::MAX; target];
 
-    for idx in 0usize..target {
-        let num = if idx < starting_numbers.len() {
-            starting_numbers[idx]
-        } else if let Some(last_idx) =  last_spoken.get(&prev_num) {
-            idx as u32 - *last_idx - 1
-        } else {
-            0
+    for (idx, num) in starting_numbers.iter().enumerate() {
+        last_spoken[*num as usize] = idx as u32;
+        prev_num = *num;
+    }
+
+    for idx in starting_numbers.len()..target {
+        let num = match last_spoken[prev_num as usize] {
+            u32::MAX => 0,
+            last_idx => (idx as u32) - last_idx - 1
         };
 
-        if idx > 0 {
-            last_spoken.insert(prev_num, (idx - 1) as u32);
-        }
+        last_spoken[prev_num as usize] = (idx as u32) - 1;
         prev_num = num;
     }
 
